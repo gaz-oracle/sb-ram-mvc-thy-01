db00540c75ebbae9688acfabcb4c00105dbd0623
gaz-oracle <fco.gaz.crespo@gmail.com>
2023-04-27 18:17:02 -0600

    1 commit inicial

---
+++ sb-ram-mvc-thy-01/src/main/java/com/gaz/app/sbrammvcthy01/SbRamMvcThy01Application.java
@@ -0,0 +1,13 @@
+package com.gaz.app.sbrammvcthy01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRamMvcThy01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRamMvcThy01Application.class, args);
+	}
+
+}
---
+++ sb-ram-mvc-thy-01/src/test/java/com/gaz/app/sbrammvcthy01/SbRamMvcThy01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaz.app.sbrammvcthy01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRamMvcThy01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
